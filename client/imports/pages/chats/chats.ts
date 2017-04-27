7df94e1a6eef6709e597c9f2d73ba684a1d478f6
CSB Lab User Account <csb-lab@fresnostate.edu>
2017-04-27 15:30:57 -0700

    End of step four

---
+++ client/imports/pages/chats/chats.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import * as Moment from 'moment';
+import { Observable } from 'rxjs';
+import { Chats, Messages } from '../../../../imports/collections';
+import { Chat, MessageType } from '../../../../imports/models';
+import template from './chats.html';
+ 
+@Component({
+  template
+})
+export class ChatsPage implements OnInit {
+  chats;
+ 
+  constructor() {
+  }
+ 
+  ngOnInit() {
+    this.chats = Chats
+      .find({})
+      .mergeMap((chats: Chat[]) =>
+        Observable.combineLatest(
+          ...chats.map((chat: Chat) =>
+            Messages
+              .find({chatId: chat._id})
+              .startWith(null)
+              .map(messages => {
+                if (messages) chat.lastMessage = messages[0];
+                return chat;
+              })
+          )
+        )
+      ).zone();
+  }
+ 
+  removeChat(chat: Chat): void {
+    Chats.remove({_id: chat._id}).subscribe(() => {});
+  }
+}
